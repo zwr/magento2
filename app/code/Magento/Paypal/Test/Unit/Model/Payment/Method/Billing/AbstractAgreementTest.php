2d24a5a86313f1109ba38adc67ea365417b50a81
Ihor Sytnykov <isitnikov@ebay.com>
2016-05-07 00:32:38 +0300

    MDVA-284: Portdown MAGETWO-51806 on merchant_beta branch

--- app/code/Magento/Paypal/Test/Unit/Model/Payment/Method/Billing/AbstractAgreementTest.php
+++ app/code/Magento/Paypal/Test/Unit/Model/Payment/Method/Billing/AbstractAgreementTest.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Copyright © 2016 Magento. All rights reserved.
+ * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Paypal\Test\Unit\Model\Payment\Method\Billing;
 
 use Magento\Framework\Object;
--- lib/internal/Magento/Framework/Model/Test/Unit/Resource/AbstractResourceStub.php
+++ lib/internal/Magento/Framework/Model/Test/Unit/Resource/AbstractResourceStub.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Copyright © 2016 Magento. All rights reserved.
+ * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Framework\Model\Test\Unit\Resource;
 
 use Magento\Framework\Object;
